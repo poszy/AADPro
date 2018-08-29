cb057d52fd582e571c106dfb8e729698edcc1cfd
Luis Pena <pena.m.luis@gmail.com>
2018-08-29 15:53:37 -0700

    Finished WhoWroteIT

--- WhoWroteIt/app/src/main/java/com/luispena/whowroteit/MainActivity.java
+++ WhoWroteIt/app/src/main/java/com/luispena/whowroteit/MainActivity.java
@@ -1,8 +1,12 @@
 package com.luispena.whowroteit;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -31,6 +35,31 @@ public class MainActivity extends AppCompatActivity {
 
         // bInput  returns an "Editable" datatype which needs to be converted into a string.
         String queryString = bInput.getText().toString();
+
+
+        // Hide the keyboard when the button is pressed
+        InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length()!=0) {
+            new FetchBook(bInput, bAuthor).execute(queryString);
+            bAuthor.setText("");
+            bTitle.setText(R.string.loading);
+        }
+
+        else {
+            if (queryString.length() == 0) {
+                bAuthor.setText("");
+                bTitle.setText("Please enter a search term");
+            } else {
+                bAuthor.setText("");
+                bTitle.setText("Please check your network connection and try again.");
+            }
+        }
+
 
         // you should not make a network connection on the UI thread.
         // If you attempt a network connection on the UI thread,
