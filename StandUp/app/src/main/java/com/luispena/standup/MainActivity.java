9166e59a451075dac8ec7508d45817be62900a80
Luis Pena <pena.m.luis@gmail.com>
2018-09-12 08:55:23 -0700

    Phase 1 of Standup App Complete

--- StandUp/app/src/main/java/com/luispena/standup/MainActivity.java
+++ StandUp/app/src/main/java/com/luispena/standup/MainActivity.java
@@ -2,6 +2,10 @@ package com.luispena.standup;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ToggleButton mToggleButton;
     private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         // Myclass b = (MyClass) a
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
+
+
         mToggleButton = (ToggleButton) findViewById(R.id.alarmToggle);
 
         mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -29,12 +36,14 @@ public class MainActivity extends AppCompatActivity {
                 String toastMessage;
 
                 if(isChecked){
+                    deliverNotification(MainActivity.this);
 
                     toastMessage = getString(R.string.alarm_on_toast);
 
                 } else {
 
                     toastMessage = getString(R.string.alarm_off_toast);
+                    mNotificationManager.cancelAll();
 
                 }
 
@@ -43,6 +52,30 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
+
+    } // end on create
+
+
+
+    private void deliverNotification(Context context){
+
+
+        Intent contentIntent = new Intent(context ,MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_text))
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+
+
 
     }
 }
