f2034b0fbfaa37df0bd4856a54605fe973edaef2
Luis Pena <pena.m.luis@gmail.com>
2018-11-29 11:49:52 -0800

    Completed Task 1 For FragmentCommunicate

---
+++ Android Devlopers Advance Course V2/FragmentCommunicate/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.fragmentexample;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass to display
+ * radio buttons.
+ */
+
+public class SimpleFragment extends Fragment {
+
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using only 0 and 1.
+    private static final int YES = 0;
+    private static final int NO = 1;
+    private static final int NONE = 2;
+    public int mRadioButtonChoice = NONE;
+    private static final String CHOICE = "choice";
+    OnFragmentInteractionListener mListener;
+
+
+    public SimpleFragment() {}
+    interface OnFragmentInteractionListener {void onRadioButtonChoice(int choice); }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment.
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+
+        if (getArguments().containsKey(CHOICE)) {
+            // A choice was made, so get the choice.
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            // Check the radio button choice.
+            if (mRadioButtonChoice != NONE) {
+                radioGroup.check
+                        (radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
+        }
+
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView = rootView.findViewById(R.id.fragment_header);
+
+                        switch (index) {
+                            case YES: // User chose "Yes".
+                                textView.setText(R.string.yes_message);
+                                mRadioButtonChoice = YES;
+                                mListener.onRadioButtonChoice(YES);
+                                break;
+                            case NO: // User chose "No".
+                                textView.setText(R.string.no_message);
+                                mRadioButtonChoice = NO;
+                                mListener.onRadioButtonChoice(NO);
+                                break;
+                            default: // No choice made.
+                                mRadioButtonChoice = NONE;
+                                mListener.onRadioButtonChoice(NONE);
+                                break;
+                        }
+                    }
+                });
+
+        // Return the View for the fragment's UI.
+        return rootView;
+    }
+
+    // This method is called as soon s the fragment is associated with the activity
+    // this code makes sure that the host Activity has implemeted the callback interface, if not error out.
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener){
+            mListener = (OnFragmentInteractionListener) context;
+        } else{
+
+            throw new ClassCastException(context.toString() +
+                    getResources().getString(R.string.exception_message));
+        }
+    }
+    /*
+     * Instantiate and return teh fragment to the activity
+     * */
+    public static SimpleFragment newInstance(int choice){
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(CHOICE, choice);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+}
