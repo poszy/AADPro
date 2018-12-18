9c6f8cf92c44e65e6e3c657e88f9f72afcd3ebf2
Luis Pena <pena.m.luis@gmail.com>
2018-12-17 16:05:45 -0800

    Finished WordRoomSample v1

--- Android Devlopers Advance Course V2/RoomWordSample/app/src/main/java/com/example/luispena/roomwordsample/WordDao.java
+++ Android Devlopers Advance Course V2/RoomWordSample/app/src/main/java/com/example/luispena/roomwordsample/WordDao.java
@@ -3,6 +3,7 @@ package com.example.luispena.roomwordsample;
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 @Dao
 public interface WordDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
 
     @Query("DELETE FROM word_table")
--- Android Devlopers Advance Course V2/RoomWordSample/app/src/main/java/com/example/luispena/roomwordsample/WordRoomDatabase.java
+++ Android Devlopers Advance Course V2/RoomWordSample/app/src/main/java/com/example/luispena/roomwordsample/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.example.luispena.roomwordsample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 // There is only one entity (table)
 @Database(entities = {Word.class}, version = 1)
@@ -16,6 +19,14 @@ public abstract class WordRoomDatabase extends RoomDatabase {
 
     private static WordRoomDatabase INSTANCE;
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();        }
+    };
+
     public static WordRoomDatabase getDatabase( final Context context){
 
         if (INSTANCE == null) {
@@ -26,10 +37,38 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database")
                             // Wipes and rebuilds instead of migrating, if no Migration object. Migration is not part of this practical.
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return  INSTANCE;
+    }
+
+    /**
+     * Populate the database in the background.
+     */
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+        String[] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAll();
+
+            for (int i = 0; i <= words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
     }
 }
