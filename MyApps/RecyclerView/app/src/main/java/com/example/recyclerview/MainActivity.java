d449896b7c8a59bc6a38201de165b5342cbbf854
Luis Pena <pena.m.luis@gmail.com>
2019-03-10 03:34:57 -0700

    redid RecyclerDemo with Click Listener

---
+++ MyApps/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    // 1. List
+    // wordlist_item is a view holder for each word
+    // 2. Adapter conects List items (1.)to view (2.)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+// Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+// Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+// Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
