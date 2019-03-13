389f71db7b1516b2ce5f9c3af0e1a43b7b4e18cc
Luis Pena <pena.m.luis@gmail.com>
2019-03-12 23:59:07 -0700

    finsished sunshine 1.2

--- DevelopersUdacity/Sunshine/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ DevelopersUdacity/Sunshine/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,10 +17,15 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.sunshine.utilities.SunshineDateUtils;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    TextView mWeatherDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +33,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         // TODO (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (3) Create an array of Strings that contain fake weather data
-
+        String[] mWeatherData = SunshineWeatherUtils.dummyWeatherData;
         // TODO (4) Append each String from the fake weather data array to the TextView
+
+        for(String mIterator: mWeatherData){
+            mWeatherDisplay.append(mIterator + "/n/n/n");
+        }
+
     }
 }
--- DevelopersUdacity/Sunshine/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/utilities/SunshineWeatherUtils.java
+++ DevelopersUdacity/Sunshine/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/utilities/SunshineWeatherUtils.java
@@ -29,6 +29,22 @@ import com.example.android.sunshine.data.SunshinePreferences;
 public final class SunshineWeatherUtils {
 
     private static final String LOG_TAG = SunshineWeatherUtils.class.getSimpleName();
+    public static final String[] dummyWeatherData = {
+            "Today, May 17 - Clear - 17°C / 15°C",
+            "Tomorrow - Cloudy - 19°C / 15°C",
+            "Thursday - Rainy- 30°C / 11°C",
+            "Friday - Thunderstorms - 21°C / 9°C",
+            "Saturday - Thunderstorms - 16°C / 7°C",
+            "Sunday - Rainy - 16°C / 8°C",
+            "Monday - Partly Cloudy - 15°C / 10°C",
+            "Tue, May 24 - Meatballs - 16°C / 18°C",
+            "Wed, May 25 - Cloudy - 19°C / 15°C",
+            "Thu, May 26 - Stormy - 30°C / 11°C",
+            "Fri, May 27 - Hurricane - 21°C / 9°C",
+            "Sat, May 28 - Meteors - 16°C / 7°C",
+            "Sun, May 29 - Apocalypse - 16°C / 8°C",
+            "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
+    };
 
     /**
      * This method will convert a temperature from Celsius to Fahrenheit.
