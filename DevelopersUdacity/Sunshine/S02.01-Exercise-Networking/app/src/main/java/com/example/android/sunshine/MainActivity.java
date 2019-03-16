930753b05c62df837867925968f30e9598a824aa
Luis Pena <pena.m.luis@gmail.com>
2019-03-15 23:28:40 -0700

    Fixed Sunshine 2.1

--- DevelopersUdacity/Sunshine/S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ DevelopersUdacity/Sunshine/S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -21,7 +21,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,7 +33,6 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
-    private TextView mSearchWeather;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,48 +43,51 @@ public class MainActivity extends AppCompatActivity {
          * do things like set the text of the TextView.
          */
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
-        mSearchWeather = (EditText) findViewById(R.id.et_weather_data);
-        // T (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
-        // T (3) Delete the for loop that populates the TextView with dummy data
 
-        // T Call loadWeatherData to perform the network request to get the weather
         loadWeatherData();
     }
 
     // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
     public void loadWeatherData(){
 
-        String weatherQuery = mSearchWeather.getText().toString();
-        URL weatherURL = NetworkUtils.buildUrl(weatherQuery);
-        mWeatherTextView.setText(weatherURL.toString());
-        new WeatherTask().execute(weatherURL);
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new WeatherTask().execute(location);
 
     }
 
     // TODO (5) Create a class that extends AsyncTask to perform network requests
-    public class WeatherTask extends AsyncTask<URL, Void, String>{
+    public class WeatherTask extends AsyncTask<String, Void, String[]> {
 
         @Override
-        protected String doInBackground(URL... params) {
-            URL searchURL = params[0];
-            String weatherResults = null;
+        protected String[] doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
+            }
+
+            String location = params[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
             try {
-                weatherResults = NetworkUtils.getResponseFromHttpUrl(searchURL);
+                String weatherResultsJson= NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl );
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, weatherResultsJson);
+                return simpleJsonWeatherData;
 
-            }catch (IOException e){
+            } catch (Exception e) {
                 e.printStackTrace();
+                return null;
             }
-            return weatherResults;
+
         }
 
         @Override
-        protected void onPostExecute(String weatherResults) {
+        protected void onPostExecute(String[] weatherResultss) {
 
-            if(weatherResults != null & !weatherResults.equals("")){
-                mWeatherTextView.setText(weatherResults);
+            if(weatherResultss != null){
+                for (String weatherString : weatherResultss) {
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
+                }
             }
-
         }
     }
-    }
+}
 
