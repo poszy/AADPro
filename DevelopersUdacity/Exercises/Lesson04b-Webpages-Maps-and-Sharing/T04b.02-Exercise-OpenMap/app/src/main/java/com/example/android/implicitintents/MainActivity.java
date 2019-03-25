1e4adc91929e16195061516de7d830f2d8422a99
Luis Pena <pena.m.luis@gmail.com>
2019-03-25 11:58:11 -0700

    Added exercise 4.2B

--- DevelopersUdacity/Exercises/Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ DevelopersUdacity/Exercises/Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -49,8 +49,24 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
+        String addressString = "1600 Amphitheatre Parkway, CA";
 
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo");
+        builder.path("0,0");
+        builder.query(addressString);
+        Uri addressUri = builder.build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(addressUri);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+
+
 
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
         Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
