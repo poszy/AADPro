e98f421f66c5cdc8d3901f75916e10855c3f8498
Luis Pena <pena.m.luis@gmail.com>
2019-03-31 23:32:49 -0700

    added exercise 6.3

--- DevelopersUdacity/Exercises/Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ DevelopersUdacity/Exercises/Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -18,11 +18,13 @@ package android.example.com.visualizerpreferences;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.example.com.visualizerpreferences.AudioVisuals.AudioInputReader;
 import android.example.com.visualizerpreferences.AudioVisuals.VisualizerView;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -42,19 +44,23 @@ public class VisualizerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizer);
         mVisualizerView = (VisualizerView) findViewById(R.id.activity_visualizer);
-        defaultSetup();
+        setupSahredPreferences();
         setupPermissions();
     }
 
     // TODO (1) Change the name of default setup to setupSharedPreferences
-    private void defaultSetup() {
+    private void setupSahredPreferences() {
         // TODO (2) Get a reference to the default shared preferences from the PreferenceManager class
+
+        SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         // TODO (3) Get the value of the show_bass checkbox preference and use it to call setShowBass
-        mVisualizerView.setShowBass(true);
+        mVisualizerView.setShowBass(mPreferences.getBoolean("show_bass",true));
         mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
         mVisualizerView.setColor(getString(R.string.pref_color_red_value));
+
     }
 
     /**
