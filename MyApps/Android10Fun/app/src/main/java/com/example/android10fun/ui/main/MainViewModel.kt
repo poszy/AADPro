fb2f7532a0e4e8cdc050973ade765d0ad77b3638
Luis Pena <pena.m.luis@gmail.com>
2019-10-15 15:24:00 -0700

    Added YARV yet another recycler view in kotlin

---
+++ MyApps/Android10Fun/app/src/main/java/com/example/android10fun/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.android10fun.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ MyApps/YARV/app/src/main/java/com/example/yarv/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.yarv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    // YARV: Yet another Recycler View.
+    // Recycler view in kotlin.
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
